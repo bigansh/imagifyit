7674f36c14ab88093835a2de63d49059e41bc683
bigansh_ <anshpushti2001@gmail.com>
2021-05-26 20:00:10 +0530

    add cloudinary config

---
+++ connections/cloudinaryConnect.js
@@ -0,0 +1,13 @@
+const cloudinary = require('cloudinary').v2,
+	dotenv = require('dotenv').config()
+
+const cloudConnect = () => {
+	cloudinary.config({
+		cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+		api_key: process.env.CLOUDINARY_API_KEY,
+		api_secret: process.env.CLOUDINARY_API_SECRET,
+	})
+    console.log(cloudinary)
+}
+
+module.exports = cloudConnect
