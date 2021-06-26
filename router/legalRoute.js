ebfe4fbb7bd9021314fa4e37f12d7b4a3f81f07c
bigansh_ <anshpushti2001@gmail.com>
2021-06-26 23:01:27 +0530

    make frontend changes

--- router/legalRoute.js
+++ router/legalRoute.js
@@ -1,10 +1,6 @@
 const express = require('express'),
 	router = express.Router()
 
-router.get('/terms-of-service', (_req, res) => {
-	res.render('tos')
-})
-
 router.get('/privacy-policy', (_req, res) => {
 	res.render('privacy')
 })
