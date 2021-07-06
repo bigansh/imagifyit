f5c45f83b16d650197c86e8e2b490f36d37a5a2c
laminarss <narasimhak453@gmail.com>
2021-07-06 08:39:05 +0530

    updated uploader block

--- public/scripts/upload.js
+++ public/scripts/upload.js
@@ -1,5 +1,6 @@
 const uploader = document.getElementById('uploader')
 const uploader_input = document.getElementById('uploader-input')
+const uploader_text = document.getElementById('uploader-text')
 const upload_form = document.getElementById('upload-form')
 const base64URL = document.getElementById('base64URL')
 
@@ -10,13 +11,17 @@ uploader.addEventListener('click', e => {
 const reader = new FileReader()
 
 uploader_input.addEventListener('change', () => {
+	uploader_text.innerText = 'Uploading...'
 	reader.readAsDataURL(uploader_input.files[0])
 
 	reader.onload = (event) => {
 		const result = event.target.result
 
 		base64URL.value = result
-
+		setTimeout(() =>{
+			uploader_text.innerText = 'Image uploaded'
+		}, 5000)
+		
 		upload_form.submit()
 	}
 })
