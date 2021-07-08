d1cddf9e0e7ac8d48f05afedb61160aa9ed557b6
laminarss <narasimhak453@gmail.com>
2021-07-08 23:56:18 +0530

    added drag-n-drop file upload

--- public/scripts/upload.js
+++ public/scripts/upload.js
@@ -1,27 +1,53 @@
-const uploader = document.getElementById('uploader')
-const uploader_input = document.getElementById('uploader-input')
-const uploader_text = document.getElementById('uploader-text')
-const upload_form = document.getElementById('upload-form')
-const base64URL = document.getElementById('base64URL')
+const uploader = document.getElementById("uploader");
+const uploader_input = document.getElementById("uploader-input");
+const uploader_text = document.getElementById("uploader-text");
+const upload_form = document.getElementById("upload-form");
+const base64URL = document.getElementById("base64URL");
 
-uploader.addEventListener('click', e => {
-	uploader_input.click();
+//click on upload area
+
+uploader.addEventListener("click", (e) => {
+  uploader_input.click();
 });
 
-const reader = new FileReader()
+uploader_input.addEventListener("change", (e) => {
+  uploadImage(uploader_input.files[0]);
+});
 
-uploader_input.addEventListener('change', () => {
-	uploader_text.innerText = 'Uploading...'
-	reader.readAsDataURL(uploader_input.files[0])
+//drag and drop
 
-	reader.onload = (event) => {
-		const result = event.target.result
+uploader.addEventListener("dragover", (e) => {
+  e.preventDefault();
+  uploader.style.borderStyle = "dashed";
+});
 
-		base64URL.value = result
-		setTimeout(() =>{
-			uploader_text.innerText = 'Image uploaded'
-		}, 5000)
-		
-		upload_form.submit()
-	}
-})
+uploader.addEventListener("dragleave", (e) => {
+  e.preventDefault();
+  uploader.style.borderStyle = "solid";
+});
+
+uploader.addEventListener("drop", (e) => {
+  e.preventDefault();
+
+  if (e.dataTransfer.files.length) {
+    uploadImage(e.dataTransfer.files[0]);
+  }
+});
+
+function uploadImage(image) {
+  uploader_text.innerText = "Uploading...";
+
+  const reader = new FileReader();
+  reader.readAsDataURL(image);
+
+  reader.onload = (event) => {
+    const result = event.target.result;
+
+    base64URL.value = result;
+    setTimeout(() => {
+      uploader_text.innerText = "Image uploaded";
+    }, 5000);
+
+    upload_form.submit();
+  };
+}
