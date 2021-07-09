39a215cb8de7aa66f3726f13c1afbbfcf91237a9
bigansh_ <anshpushti2001@gmail.com>
2021-07-09 17:41:38 +0530

    back to basic

--- public/scripts/upload.js
+++ public/scripts/upload.js
@@ -1,26 +1,18 @@
-const main_form = document.getElementById('main-form'),
-	uploader_form = document.getElementById('uploader-form'),
-	resizer_form = document.getElementById('resizer-form')
+const main_form = document.getElementById('main-form')
 
 const uploader_div = document.getElementById('uploader-div'),
 	file = document.getElementById('file'),
 	base64URL = document.getElementById('base64URL'),
 	uploader_url = document.getElementById('img-url')
 
-const canvas = document.getElementById('canvas')
-
-const reader = new FileReader(),
-	img = new Image()
+const reader = new FileReader()
 
 uploader_div.addEventListener('click', (e) => {
 	file.click()
 })
 
 file.addEventListener('change', (e) => {
-	uploadImage(file.files[0])
-
-	uploader_form.classList.toggle('d-none')
-	resizer_form.classList.toggle('d-none')
+	toBase64(file.files[0])
 })
 
 uploader_div.addEventListener('dragover', (e) => {
@@ -34,34 +26,12 @@ uploader_div.addEventListener('dragleave', (e) => {
 uploader_div.addEventListener('drop', (e) => {
 	e.preventDefault()
 
-	if (e.dataTransfer.files.length) {
-		uploadImage(e.dataTransfer.files[0])
-	}
-
-	uploader_form.classList.toggle('d-none')
-	resizer_form.classList.toggle('d-none')
+	if (e.dataTransfer.files.length) toBase64(e.dataTransfer.files[0])
 })
 
-uploader_url.addEventListener('change', () => {
-	uploader_form.classList.toggle('d-none')
-	resizer_form.classList.toggle('d-none')
-})
+const toBase64 = (image) => {
+	console.log(image)
 
-// file.addEventListener('change', () => {
-// 	reader.readAsDataURL(file.files[0])
-
-// 	base64URL.value = result
-// 	setTimeout(() => {
-// 		uploader_text.innerText = 'Image uploaded'
-// 	}, 5000)
-
-// 	base64URL.value = result
-
-// 	uploader_form.classList.toggle('d-none')
-// 	resizer_form.classList.toggle('d-none')
-// })
-
-const uploadImage = (image) => {
 	reader.readAsDataURL(image)
 
 	reader.onload = (e) => {
@@ -69,6 +39,6 @@ const uploadImage = (image) => {
 
 		base64URL.value = result
 
-		console.log(base64URL.value)
+		main_form.submit()
 	}
 }
