afc5cde64af7c8b5459f1e32e4ed89e1c482c85f
laminarss <narasimhak453@gmail.com>
2021-07-17 20:47:16 +0530

    make form-validation interactive

--- public/scripts/create.js
+++ public/scripts/create.js
@@ -4,7 +4,19 @@ let title_check = false,
 	alias_check = false
 
 const text_pattern = /\w+/
-const url_pattern = /^http[s]?:\/\//
+const url_pattern = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/
+
+const create_form = document.getElementById('create-form'),
+		title_input = document.getElementById('title-input'),
+		description_input = document.getElementById('description-input'),
+		url_input = document.getElementById('url-input'),
+		alias_input = document.getElementById('alias-input'),
+		btn_showcase = document.getElementById('btn-showcase')
+
+const title_error = document.getElementById('title-error'),
+		description_error = document.getElementById('description-error'),
+		url_error = document.getElementById('url-error'),
+		alias_error = document.getElementById('alias-error')
 
 const card_type = document.getElementById('select-option'),
 		small_card = document.getElementById('card-small'),
@@ -13,12 +25,101 @@ const card_type = document.getElementById('select-option'),
 const card_title = [...document.getElementsByClassName('card-title')],
 		card_description = [...document.getElementsByClassName('card-description')]
 
-const create_form = document.getElementById('create-form'),
-		title_input = document.getElementById('title-input'),
-		description_input = document.getElementById('description-input'),
-		url_input = document.getElementById('url-input'),
-		alias_input = document.getElementById('alias-input'),
-		btn_create = document.getElementById('btn-create')
+const errorStyle = (input_field, error_text) => {
+	input_field.classList.add('err')
+	error_text.classList.add('error')
+	console.log('alias-error');
+}
+
+const initialStyle = (input_field, error_text) => {
+	input_field.classList.remove('err')
+	error_text.classList.remove('error')
+	console.log('alias-initial');
+}
+
+title_input.addEventListener('input', () => {
+	card_title.forEach((title) => {
+		
+		title_check = text_pattern.test(title_input.value)
+		
+		if(title_input.value.length === 0) {
+			title_error.innerText = 'Enter Title here'
+			errorStyle(title_input, title_error)
+		}
+		else if ( !(title_check) ) {
+			title.innerHTML = 'Title'
+			title_error.innerText = 'Only alphabet, numbers and underscore are allowed'
+			errorStyle(title_input, title_error)
+		}
+		else if(title_input.value.length > 32) {
+			title_error.innerText = 'Title should not exceed 32 characters'
+			errorStyle(title_input, title_error)	
+		}
+		else {
+			title.innerHTML = title_input.value
+			initialStyle(title_input, title_error)
+		}
+	})
+})
+
+description_input.addEventListener('input', () => {
+	card_description.forEach((description) => {
+		
+		description_check = text_pattern.test(description_input.value)
+
+		if(description_input.value.length === 0) {
+			title_error.innerText = 'Enter Description here'
+			errorStyle(description_input, description_error)
+		}
+		else if ( !(description_check) ) {
+			description.innerHTML = 'Description'
+			description_error.innerText = 'Only alphabet, numbers and underscore are allowed'
+			errorStyle(description_input, description_error)
+		}
+		else if(description_input.value.length > 256) {
+			description_error.innerText = 'Description should not exceed 256 characters'
+			errorStyle(description_input, description_error)	
+		}
+		else {
+			description.innerHTML = description_input.value
+			initialStyle(description_input, description_error)
+		}
+	})
+})
+
+url_input.addEventListener('input', () => {
+	
+	url_check = url_pattern.test(url_input.value)
+	if(url_input.value.length === 0) {
+		url_error.innerText = 'Enter Destination URL here'
+	}
+	else if( !(url_check) ) {
+		url_error.innerText = 'Enter a valid URL ( Begins with http:// or https:// )'
+		errorStyle(url_input, url_error)
+	}
+	else {
+		initialStyle(url_input, url_error)
+	}
+})
+
+alias_input.addEventListener('change', async () => {
+	
+	const result = await fetch(`/api/alias-check/${alias_input.value}`, {
+		method: 'POST',
+	})
+	
+	const count = await result.json().then(res => res.count)
+	
+	alias_check =  text_pattern.test(alias_input.value) && (count === 0)
+	
+	if( !( alias_check || alias_input.value==='' ) ) {
+		alias_error.innerText = 'Alias already taken ( Try another one )'
+		errorStyle(alias_input, alias_error)
+	}
+	
+	else 
+		initialStyle(alias_input, alias_error)
+})
 
 card_type.addEventListener('change', () => {
 	if (card_type.value === 'large') {
@@ -31,82 +132,7 @@ card_type.addEventListener('change', () => {
 	}
 })
 
-// const setBorder = (type, color) => {
-// 	type.style.borderColor = color;
-// }
-
-title_input.addEventListener('input', () => {
-	card_title.forEach((title) => {
-
-		title_check = text_pattern.test(title_input.value)
-		if ( !(title_check) ) {
-			title.innerHTML = 'Title'
-			// setBorder(title_input, 'Tomato')
-			title_input.classList.add('err')
-		}
-		else {
-			title.innerHTML = title_input.value
-			// setBorder(title_input, 'MediumSeaGreen')
-			title_input.classList.remove('err')
-		}
-	})
-})
-
-description_input.addEventListener('input', () => {
-	card_description.forEach((description) => {
-
-		description_check = text_pattern.test(description_input.value)
-		if ( !(description_check) ) {
-			description.innerHTML = 'Description'
-			// description_input.style.borderColor = 'Tomato';
-			description_input.classList.add('err')
-		}
-		else {
-			description.innerHTML = description_input.value
-			// description_input.style.borderColor = 'MediumSeaGreen';
-			description_input.classList.remove('err')
-		}
-	})
-})
-
-url_input.addEventListener('input', () => {
-
-	url_check = url_pattern.test(url_input.value)
-	if( !(url_check) ) {
-		// url_input.style.borderColor = 'Tomato';
-		url_input.classList.add('err')
-	}
-	else {
-		// url_input.style.borderColor = 'MediumSeaGreen';
-		url_input.classList.remove('err')
-	}
-})
-
-alias_input.addEventListener('input', async () => {
-
-	const result = await fetch(`/api/alias-check/${alias_input.value}`, {
-		method: 'POST',
-	})
-	
-	const count = await result.json().then(res => res.count)
-	
-	alias_check =  text_pattern.test(alias_input.value) && (count === 0)
-
-	//alert('Alias already in use')
-	// if(alias_input.value !== '') {
-	if( !( alias_check && alias_input.value==='' ) ) {
-		// setBorder(alias_input, 'Tomato')
-		alias_input.classList.add('err')
-	}
-
-	//alert('Alias ok')
-	else //if(count === 0 || alias_input.value === ''){
-		// setBorder(alias_input, 'MediumSeaGreen')
-		alias_input.classList.remove('err')
-	//}
-})
-
-btn_create.addEventListener('click', () => {
+btn_showcase.addEventListener('click', () => {
 	if( title_check && description_check && url_check && (alias_check || alias_input.value === '' )) {
 		create_form.submit() 
 	}
@@ -114,16 +140,17 @@ btn_create.addEventListener('click', () => {
 		document.body.scrollTop = 0
 		document.documentElement.scrollTop = 0
 
-		if( !title_check )
-			title_input.classList.add('err')
+		if( !title_check ) {
+			errorStyle(title_input, title_error)
+		}
 
 		if( !description_check )
-			description_input.classList.add('err')
+			errorStyle(description_input, description_error)
 
 		if( !url_check ) 
-			url_input.classList.add('err')
+			errorStyle(url_input, url_error)
 
-		if( alias_check || alias_input.value !== '' )
-			alias_input.classList.add('err')
+		if( !( alias_check || alias_input.value === '' ) )
+			errorStyle(alias_input, alias_error)
 	}
 })
