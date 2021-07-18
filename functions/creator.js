81c0fac6dc4dcbaa738e15cb7f022c84d8296bb7
bigansh_ <anshpushti2001@gmail.com>
2021-07-18 13:39:19 +0530

    change few code stylings

--- functions/creator.js
+++ functions/creator.js
@@ -3,8 +3,6 @@ const OpenGraph = require('../models/ogSchema')
 const ogSchema = require('../schema/og')
 
 const creator = async (ogDetails) => {
-	console.log(ogDetails)
-
 	ogSchema.cardType = ogDetails.card
 	ogSchema.description = ogDetails.description
 	ogSchema.image = ogDetails.image
