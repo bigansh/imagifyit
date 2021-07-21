7b97f92f9be6b7e9d27d5a50a3a077a5b2fd3019
bigansh_ <anshpushti2001@gmail.com>
2021-07-21 16:17:17 +0530

    add tos and improve pp

--- router/legalRoute.js
+++ router/legalRoute.js
@@ -1,8 +1,12 @@
 const express = require('express'),
 	router = express.Router()
 
-router.get('/privacy-policy', (_req, res) => {
+router.get('/privacy-policy', (req, res) => {
 	res.render('privacy')
+})
+
+router.get('/terms-of-service', (req, res) => {
+	res.render('terms')
 })
 
 module.exports = router
